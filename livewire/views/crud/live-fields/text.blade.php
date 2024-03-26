989f1f21ecc207f640a5f53798add8cd6d933fb2
Vinicius Bassalobre <bassalobre.vinicius@gmail.com>
2024-03-26 16:12:01 -0300

    ASD

--- livewire/views/crud/live-fields/text.blade.php
+++ livewire/views/crud/live-fields/text.blade.php
@@ -6,6 +6,7 @@
     [
         'index' => data_get($field, 'field'),
         'mask' => data_get($field, 'mask', ''),
+        'disabled' => data_get($field, 'disabled', false),
         'type' => @$type ? $type : 'text',
         'crudId' => $crudId,
         'rows' => data_get($field, 'rows', 3),
