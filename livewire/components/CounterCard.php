12b68e703bae28ab3d6ca511009f96cc7d36d280
Vinicius Bassalobre <bassalobre.vinicius@gmail.com>
2024-03-27 17:27:47 -0300

    MELHORA COUNTER

--- livewire/components/CounterCard.php
+++ livewire/components/CounterCard.php
@@ -6,26 +6,32 @@ use App\Http\Supernova\Application;
 use Livewire\Component;
 use Livewire\Attributes\Lazy;
 
-#[Lazy]
 class CounterCard extends Component
 {
     public $module;
+    public $loading = true;
+    public $name = '';
+    public $qty = 0;
+    public $cardCounterReloadTime = 0;
 
-    public function placeholder()
+    public function loadData()
     {
-        return view('supernova-livewire-views::skeleton', ['size' => '136
-        px']);
+        $application = app()->make(config("supernova.application", Application::class));
+        $module = $application->getModule($this->module);
+        $this->name = $module->name()[1];
+        $this->cardCounterReloadTime = $application->cardCounterReloadTime();
+        $this->qty = $module->getQty();
+        $this->loading = false;
     }
 
     public function render()
     {
-        $application = app()->make(config("supernova.application", Application::class));
-        $module = $application->getModule($this->module);
-        $name = $module->name()[1];
-        $content = $module->getQty();
-        $cardCounterReloadTime = $application->cardCounterReloadTime();
+        if ($this->loading) return view('supernova-livewire-views::skeleton', ['size' => '136px', 'action' => "wire:init=loadData"]);
+        $cardCounterReloadTime = $this->cardCounterReloadTime;
         $actions = "wire:poll.{$cardCounterReloadTime}s";
         $route = route("supernova.modules.index", ['module' => $this->module]);
+        $name = $this->name;
+        $content = $this->qty;
         return <<<BLADE
             <a href="$route" class="bg-white dark:bg-gray-800 p-6 rounded-lg shadow-sm w-full cursor-pointer" $actions>
                 <h2 class="text-xl font-bold text-gray-800 dark:text-gray-50 mb-3">
--- livewire/views/skeleton.blade.php
+++ livewire/views/skeleton.blade.php
@@ -1,4 +1,5 @@
-<div role="status" class="w-full animate-pulse h-[{{ @$size ?? '200px' }}] cursor-wait rounded-sm {{ $class ?? '' }}">
+<div role="status" class="w-full animate-pulse h-[{{ @$size ?? '200px' }}] cursor-wait rounded-sm {{ $class ?? '' }}"
+    @if (@$action) {{ $action }} @endif>
     <div class="h-2.5 bg-gray-200 dark:bg-gray-700 w-full h-full rounded"></div>
     <span class="sr-only">Loading...</span>
 </div>
